9447a62b5c42882fe8d147e3d5d07408962f590f
vinodkumar <kumarvinod.paleti@gmail.com>
2024-02-20 16:05:39 +0530

    files are added

--- 03-mongodb/mongodb.sh
+++ 03-mongodb/mongodb.sh
@@ -2,4 +2,4 @@
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/sivadevopsdaws74s/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/vinodkumarpaleti/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
